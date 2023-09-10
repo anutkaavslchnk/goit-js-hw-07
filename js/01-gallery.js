82628a268182974c591268a48bf7ec7995297460
anutkaavslchnk <anutochkaaaaa14@gmail.com>
2023-09-10 12:37:40 +0300

    ready

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,35 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const list = document.querySelector(".gallery");
+
+const array = galleryItems.map((item, index) => {
+  return `<li class="gallery__item" data-index="${index}"><a class="gallery__link" href="${item.original}"><img src="${item.preview}" alt="${item.description}" data-source="${item.original}"   class="gallery__image" /></a></li>`;
+});
+
+list.insertAdjacentHTML("beforeend", array.join(""));
+
+list.addEventListener("click", listImg);
+
+function listImg(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+  const currentItem = event.target.closest(".gallery__item");
+  const index = currentItem.dataset.index;
+  const instance = basicLightbox.create(
+    `<div class="modal"><img src="${galleryItems[index].original}" alt="${galleryItems[index].description}"   class="gallery__image" /></div>`
+  );
+  instance.show();
+}
+
+document.addEventListener("keydown", handleKeyDown);
+
+function handleKeyDown(event) {
+  if (event.code === "Escape") {
+    instance.close();
+    window.removeEventListener("keydown", handleKeyDown);
+    
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const list = document.querySelector(".gallery");
+
+const array = galleryItems.map((item, index) => {
+  return `<li class="gallery__item" data-index="${index}"><a class="gallery__link" href="${item.original}"><img src="${item.preview}" alt="${item.description}" data-source="${item.original}"   class="gallery__image" /></a></li>`;
+});
+
+list.insertAdjacentHTML("beforeend", array.join(""));
+var lightbox = new SimpleLightbox('.gallery a', { captionsData:"alt",captionDelay:250, });
+list.addEventListener("click", listImg);
+
+function listImg(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+  const currentItem = event.target.closest(".gallery__item");
+}
